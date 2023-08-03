3492f5806d34f9e00defa7f1cb12cf1a6c4b7f43
jeremyhsimons <>
2023-08-03 10:53:14 +0000

    Add filters for profiles and posts.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
+
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    like_id = serializers.SerializerMethodField()
+    comment_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user,  post=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -31,4 +45,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title', 'content',
             'image', 'is_owner', 'profile_id', 'profile_image', 'image_filter',
+            'like_id', 'comment_count', 'likes_count',
         ]
